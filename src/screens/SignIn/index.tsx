0ffa3ce879117b83a3ebcb6cb725b665c63304be
Jhonatan mendes <jhowfrombrazil@gmail.com>
2021-06-22 12:04:28 -0300

    adding fonts and theme

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,20 +1,29 @@
-import React, { useState } from "react";
-import { View, Text, TextInput } from "react-native";
+import React from "react";
+import { View, Text, Image } from "react-native";
 import { styles } from "./styles";
+import illustrationImg from "../../assets/illustration.png";
+import { ButtonIcon } from "../../components/ButtonIcon";
 
 export const SignIn = () => {
-  const [textInput, setTextInput] = useState("");
-
   return (
     <View style={styles.container}>
-      <Text>HI NLW</Text>
-
-      <TextInput
-        style={styles.input}
-        value={textInput}
-        onChangeText={setTextInput}
+      <Image
+        resizeMode="stretch"
+        source={illustrationImg}
+        style={styles.image}
       />
-      <Text>Voce digitou : {textInput} </Text>
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Conecte-se {"\n"}e organize suas {"\n"}
+          jogatinas
+        </Text>
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {"\n"}
+          favoritos com seus amigos
+        </Text>
+        <ButtonIcon title="Entrar com Discord" activeOpacity={0.7} />
+      </View>
     </View>
   );
 };
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,14 +1,36 @@
 import { StyleSheet } from "react-native";
+import { theme } from "../../global/styles/theme";
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: theme.colors.secondary100,
   },
-  input: {
-    height: 50,
-    width: 200,
-    borderWidth: 2,
+
+  image: {
+    width: "100%",
+    height: 360,
+  },
+  content: {
+    marginTop: -40,
+    paddingHorizontal: 50,
+  },
+  title: {
+    color: theme.colors.heading,
+    textAlign: "center",
+    fontSize: 40,
+    fontFamily: theme.fonts.title700,
+    marginBottom: 16,
+    lineHeight: 40,
+  },
+  subtitle: {
+    color: theme.colors.heading,
+    fontSize: 15,
+    textAlign: "center",
+    fontFamily: theme.fonts.title500,
+    marginBottom: 64,
+    lineHeight: 25,
   },
 });
