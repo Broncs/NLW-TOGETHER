46cd24972b5c5bcd840db57e23138f4a62b167ac
Jhonatan mendes <jhowfrombrazil@gmail.com>
2021-06-23 15:45:35 -0300

    finish aula 3

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -12,15 +12,26 @@ import { theme } from "../../global/styles/theme";
 import { styles } from "./styles";
 
 import { CategorySelect } from "../../components/CategorySelect";
-import { Background } from "../../components/Background";
+import { ModalView } from "../../components/ModalView";
 import { GuildIcon } from "../../components/GuildIcon";
 import { Header } from "../../components/Header";
 import { SmallInput } from "../../components/SmallInput";
 import { TextArea } from "../../components/TextArea";
 import { Button } from "../../components/Button";
+import { Guilds } from "../Guilds";
+import { GuildProps } from "../../components/Guild";
 
 export const AppointmentCreate = () => {
   const [category, setCategory] = useState("");
+  const [openGuildsModal, setOpenGuildsModal] = useState(false);
+  const [guild, setGuild] = useState<GuildProps>({} as GuildProps);
+
+  const handleOpenGuildsModal = () => setOpenGuildsModal(true);
+
+  const handleGuildSelect = (guildSelect: GuildProps) => {
+    setGuild(guildSelect);
+    setOpenGuildsModal(false);
+  };
 
   return (
     <KeyboardAvoidingView
@@ -45,15 +56,14 @@ export const AppointmentCreate = () => {
         />
 
         <View style={styles.form}>
-          <RectButton>
+          <RectButton onPress={handleOpenGuildsModal}>
             <View style={styles.select}>
-              {
-                // <View style={styles.image} />
-                <GuildIcon />
-              }
+              {guild.icon ? <GuildIcon /> : <View style={styles.image} />}
 
               <View style={styles.selectBody}>
-                <Text style={styles.label}>Selecione um servidor</Text>
+                <Text style={styles.label}>
+                  {guild.name ? guild.name : "Selecione um servidor"}
+                </Text>
               </View>
               <Feather
                 name="chevron-right"
@@ -99,6 +109,9 @@ export const AppointmentCreate = () => {
           </View>
         </View>
       </ScrollView>
+      <ModalView visible={openGuildsModal}>
+        <Guilds handleGuildSelect={handleGuildSelect} />
+      </ModalView>
     </KeyboardAvoidingView>
   );
 };
